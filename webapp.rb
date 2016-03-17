b73882323c646de2080b64759c2fc28cb48d9be3
wellsonjain <wellsonwei@ecoworkinc.com>
2016-03-18 10:18:04 +1100

    Implement webhook to trello api
    Signed-off-by: wellsonjain <wellsonwei@ecoworkinc.com>

--- webapp.rb
+++ webapp.rb
@@ -2,21 +2,25 @@ require 'sinatra'
 require 'json'
 require "net/https"
 require "uri"
+require 'dotenv'
+require "trello"
+
+Dotenv.load
+
+Trello.configure do |trello|
+  trello.developer_public_key = ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
+  trello.member_token = ENV['TRELLO_MEMBER_TOKEN']
+end
 
 before do
   request.body.rewind
   @request_payload = JSON.parse request.body.read
 end
 
-post '/gitlab/webhook' do
-  user_name = @request_payload["user_name"]
-  repository_name = @request_payload["repository"]["name"]
-  commits = @request_payload["commits"]
-
-  title = "#{user_name} pushed to git.42la.bs #{repository_name}"
-  messages = '<ul>' + commits.map do |commit|
-    "<li>#{commit['message']}</li>"
-  end.join + '</ul>'
-
-  puts messages
+post '/webhook' do
+  card = Trello::Card.create(
+    name: @request_payload['issue']['title'],
+    list_id: ENV['MY_TARGET_LIST'],
+    member_ids: [ Trello::Member.find(ENV['USER']).id ]
+  )
 end
